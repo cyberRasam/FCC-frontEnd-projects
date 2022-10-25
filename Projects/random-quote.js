8369028bf5bf8533594794c26616ce7dfc3f5af6
cyberrasam <you@example.com>
2022-10-26 01:17:43 +0300

    app working

---
+++ Projects/random-quote.js
@@ -0,0 +1,78 @@
+let quotes = [
+  {
+    quote: 'Life isn’t about getting and having, it’s about giving and being.',
+    author: 'Kevin Kruse',
+  },
+  {
+    quote: 'Whatever the mind of man can conceive and believe, it can achieve.',
+    author: 'Napoleon Hill',
+  },
+  {
+    quote: 'Strive not to be a success, but rather to be of value.',
+    author: 'Albert Einstein',
+  },
+];
+
+var colors = [
+  '#16a085',
+  '#27ae60',
+  '#2c3e50',
+  '#f39c12',
+  '#e74c3c',
+  '#9b59b6',
+  '#FB6964',
+  '#342224',
+  '#472E32',
+  '#BDBB99',
+  '#77B1A9',
+  '#73A857',
+];
+
+var currentQuote = '',
+  currentAuthor = '';
+
+function getRandomQuote() {
+  return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+function getQuote() {
+  let randomQuote = getRandomQuote();
+
+  currentQuote = randomQuote.quote;
+  currentAuthor = randomQuote.author;
+
+  $('#tweet-quote').attr(
+    'href',
+    'https://twitter.com/intent/tweet?hashtags=quotes&related=freecodecamp&text=' +
+      encodeURIComponent('"' + currentQuote + '" ' + currentAuthor)
+  );
+
+  $('.quote-text').animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 500);
+    $('#text').text(randomQuote.quote);
+  });
+
+  $('.quote-author').animate({ opacity: 0 }, 500, function () {
+    $(this).animate({ opacity: 1 }, 500);
+    $('#author').html(randomQuote.author);
+  });
+
+  var color = Math.floor(Math.random() * colors.length);
+  $('html body').animate(
+    {
+      backgroundColor: colors[color],
+      color: colors[color],
+    },
+    1000
+  );
+  $('.button').animate(
+    {
+      backgroundColor: colors[color],
+    },
+    1000
+  );
+}
+
+$(document).ready(function () {
+  $('#new-quote').on('click', getQuote);
+});
---
+++ README.md
@@ -0,0 +1,14 @@
+# FCC-frontEnd-projects
+
+During the Freecodecamp.org Front End Development Libraries curriculum I learned to work with 
+
+[
+Bootstrap {https://www.freecodecamp.org/learn/front-end-development-libraries/#bootstrap},
+Jquery {https://www.freecodecamp.org/learn/front-end-development-libraries/#jquery}, 
+Sass {https://www.freecodecamp.org/learn/front-end-development-libraries/#sass},
+React {https://www.freecodecamp.org/learn/front-end-development-libraries/#react},
+Redux {https://www.freecodecamp.org/learn/front-end-development-libraries/#redux},
+React and Redux {https://www.freecodecamp.org/learn/front-end-development-libraries/#react-and-redux}
+]
+
+By clicking on the links you can see subjects of each technology and problem examples I've solved. 
